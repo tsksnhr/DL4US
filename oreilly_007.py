482720604861ca6bf5293717b4a6f281a8d4fdca
tsksnhr <toshiki.o3o5.s@gmail.com>
2019-11-03 16:40:13 +0900

    create soft-max function

--- oreilly_007.py
+++ oreilly_007.py
@@ -4,7 +4,8 @@ import numpy as np
 
 def soft_max(x):
 
-    a = np.exp(x)
+    x_max = max(x)
+    a = np.exp(x-x_max)
     a_sum = sum(a)
 
     y = a/a_sum
@@ -13,8 +14,10 @@ def soft_max(x):
 
 if __name__ == '__main__':
 
-    x_ = np.array([1, 2, 3])
+    x_ = np.array([1000, 1200, 1100])
     out = soft_max(x_)
 
     print(x_)
     print(out)
+
+    print(sum(out))
