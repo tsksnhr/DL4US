99140a32ec6b3201e0d271d738e7600425106ebb
tsksnhr <toshiki.o3o5.s@gmail.com>
2019-11-03 18:21:15 +0900

    edited to call as a module

--- load_data.py
+++ load_data.py
@@ -4,7 +4,7 @@ import gzip
 import pickle
 import matplotlib.pyplot as plt
 
-def load_image_mnist(value):
+def load_image_mnist(datasets_path, value):
 
     # file path
     file_path = os.path.join(datasets_path, value)
@@ -17,7 +17,8 @@ def load_image_mnist(value):
 
     return image
 
-def load_label_mnist(value):
+
+def load_label_mnist(datasets_path, value):
 
     # file path
     file_path = os.path.join(datasets_path, value)
@@ -28,7 +29,11 @@ def load_label_mnist(value):
 
     return label
 
-if __name__ == "__main__":
+
+def load_mnist_datasets_as_pkl():
+
+    # directory path (in other env, change path)
+    datasets_path = os.path.join('C:\\', 'Users', 'chest', 'Desktop', 'python', 'Learn_DL4US', 'datasets')
 
     # set file-name
     file_dic = {}
@@ -37,22 +42,15 @@ if __name__ == "__main__":
     file_dic['test_images'] = 't10k-images-idx3-ubyte.gz'
     file_dic['test_labels'] = 't10k-labels-idx1-ubyte.gz'
 
-    # directory path  (choose your username)
-    # datasets_path = os.path.join('C:\\', 'Users', 'toshi', 'Desktop', 'python', 'Learn_DL4US', 'datasets')
-    datasets_path = os.path.join('C:\\', 'Users', 'chest', 'Desktop', 'python', 'Learn_DL4US', 'datasets')
-
-    # show dataset as a list
-    print(load_image_mnist(file_dic['train_images']))
-    print(load_label_mnist(file_dic['train_labels']))
-    print(load_image_mnist(file_dic['test_images']))
-    print(load_label_mnist(file_dic['test_labels']))
-
     # assign datasets to dictionary
     dataset_dic = {}
-    dataset_dic['train_images'] = load_image_mnist(file_dic['train_images'])
-    dataset_dic['train_labels'] = load_label_mnist(file_dic['train_labels'])
-    dataset_dic['test_images'] = load_image_mnist(file_dic['test_images'])
-    dataset_dic['test_labels'] = load_label_mnist(file_dic['test_labels'])
+    dataset_dic['train_images'] = load_image_mnist(datasets_path, file_dic['train_images'])
+    dataset_dic['train_labels'] = load_label_mnist(datasets_path, file_dic['train_labels'])
+    dataset_dic['test_images'] = load_image_mnist(datasets_path, file_dic['test_images'])
+    dataset_dic['test_labels'] = load_label_mnist(datasets_path, file_dic['test_labels'])
+
+    # show datasets (use for debag)
+    print(dataset_dic)
 
     # save datasets dictionary as pkl
     as_pickle = os.path.join(datasets_path, "mnist.pkl")
@@ -63,12 +61,21 @@ if __name__ == "__main__":
     with open(as_pickle, "rb") as fp:
         pkl_datasets = pickle.load(fp)
 
+    return pkl_datasets
+
+
+if __name__ == "__main__":
+
+    pkl_datasets = load_mnist_datasets_as_pkl()
+
     # number of files
     print(pkl_datasets['train_images'].shape)
     print(pkl_datasets['train_labels'].shape)
+    print(pkl_datasets['test_images'].shape)
+    print(pkl_datasets['test_labels'].shape)
 
     # show graphic
     gra_train_img = pkl_datasets["train_images"][144].reshape((28, 28))
     plt.imshow(gra_train_img)
     plt.show()
-    print(pkl_datasets["train_labels"][144])
+    print("label is", pkl_datasets["train_labels"][144])
