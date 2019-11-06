51458ebb4474a22014815eb0bceb88faf9cbb687
tsksnhr <toshiki.o3o5.s@gmail.com>
2019-11-06 23:28:56 +0900

    edited cross_entropy_error
    to apply cross-entropy-error to batch data and/or not one-hot label

--- loss_func.py
+++ loss_func.py
@@ -1,11 +1,23 @@
 import numpy as np
 
 def mean_square_error(test_x, pre_y):
-    return 0.5*sum((pre_y-test_x)**2)
+    return 0.5*np.sum((pre_y-test_x)**2)
 
-def cross_entropy_error(test_x, pre_y):
+def cross_entropy_error(test_x, pre_y, one_hot=True):
+
+    if pre_y.ndim == 1:
+        test_x = test_x.reshape(1, test_x.size)
+        pre_y = pre_y.reshape(1, pre_y.size)
+
+    batch_size = pre_y.shape[0]
     delta = 1e-7
-    return (-1)*sum(test_x*np.log(pre_y+delta))
+
+    if one_hot:
+        loss = (-1)*np.sum(test_x*np.log(pre_y+delta))/batch_size
+    else:
+        loss = (-1)*np.sum(np.log(pre_y[np.arange(batch_size), test_x]+delta))/batch_size
+    return loss
+    
 
 if __name__ == "__main__":
 
