eb0c298aafb589863489b910c12e2ad49361adc3
tsksnhr <toshiki.o3o5.s@gmail.com>
2019-11-17 15:38:50 +0900

    edited internal function
    to utilize ndarray including 2d array

--- numerical_gradient.py
+++ numerical_gradient.py
@@ -5,8 +5,11 @@ def numerical_gradient(f, x):
     delta = 1e-6
     grad = np.zeros_like(x)
 
-    for index in range(np.size(x)):
-        
+    it = np.nditer(x, flags=["multi_index"], op_flags=["readwrite"])
+
+    while not it.finished:
+        index = it.multi_index
+
         # calc f(x+h)
         temp = x[index]
         x[index] = temp + delta 
@@ -21,14 +24,16 @@ def numerical_gradient(f, x):
 
         # calc gradient
         grad[index] = (f1-f2)/(2*delta)
+        it.iternext()
 
     return grad
 
 if __name__ == "__main__":
 
     def func(x):
-        return x[0]**2 + x[1]**2
+        return x[0,0]**2 + x[0,1]**2 + x[1,0]**2 + x[1,1]**2
 
-    x = np.array([3.0, 4.0])            # [3, 4] makes overflow since grad = (int)/(float) 
+#    x = np.array([3.0, 4.0])            # [3, 4] makes overflow since grad = (int)/(float) 
+    x = np.array([[1.0, 2.0], [3.0, 4.0]])
     grad = numerical_gradient(func, x)
     print(grad)
